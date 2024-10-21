bfd64db619d42fcdd0617e4af73733ea047db147
VeraK <vera.krapivner@gmail.com>
2024-10-21 19:30:06 +0200

    Changes princess sprite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+	
     public Animator animator;
     public float moveSpeed = 1.0f;
     private Rigidbody2D rb; //Rb for the player
@@ -24,46 +24,56 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
 		float moveX = Input.GetAxisRaw("Horizontal");
 
-        movement = new Vector2(moveX, 0);//(x,0)
-
+		movement = new Vector2(moveX, 0);//(x,0)
         animator.SetFloat("SpeedParam", Mathf.Abs(movement.x));//Trigger animation
 
         if (moveX > 0)
-            //transform.localScale = new Vector3(1, 1, 1); //Facing right
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);  
         else if (moveX < 0)
-			//transform.localScale = new Vector3(-1, 1, 1);//Facing left
 			transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
 
-		if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+		Debug.Log("isGrounded " + isGrounded);
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
 		{
-			Debug.Log("Enter if statement");
-			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+			Debug.Log("Entered Space");
+			rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+			Debug.Log("Player jumped");
+			animator.SetBool("isJumping", true);
+			animator.SetBool("isGrounded", false);
 		}
+
 	}
 
 	private void FixedUpdate()
 	{
-        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+
+		rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y); // Update horizontal velocity, but keep vertical velocity
+
 	}
 
-	private void OnTriggerEnter2D(Collider2D collision)
+
+	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		if(collision.CompareTag("Ground"))
-        {
-            isGrounded = true;
+		Debug.Log("Enter");
+		if (collision.collider.CompareTag("Ground"))
+		{
+			isGrounded = true;
+			animator.SetBool("isGrounded", true);
+			animator.SetBool("isJumping", false);
 			Debug.Log("Player is grounded");
 		}
 	}
 
-	private void OnTriggerExit2D(Collider2D collision)
+	private void OnCollisionExit2D(Collision2D collision)
 	{
-		if(collision.CompareTag("Ground"))
+		Debug.Log("Exit");
+		if (collision.collider.CompareTag("Ground"))
 		{
 			isGrounded = false;
+			animator.SetBool("isGrounded", false);
 			Debug.Log("Player is not grounded");
 		}
 	}
